3f08b6ebebe82a99234ef272936e37621912b6ea
简尚全 <jianshangquan3@gmail.com>
2022-01-26 17:49:50 +0630

    complete nav bar

--- Web3.0 sample app/client/src/App.jsx
+++ Web3.0 sample app/client/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+import {Navbar, Welcome, Footer, Services, Transaction} from './components'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services />
+      <Transaction />
+      <Footer />
     </div>
   )
 }
