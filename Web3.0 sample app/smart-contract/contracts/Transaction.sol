cc1b37ed06dac066427c6f395d0446be87a36529
简尚全 <jianshangquan3@gmail.com>
2022-01-27 16:27:39 +0630

    add smart contract code solidity

---
+++ Web3.0 sample app/smart-contract/contracts/Transaction.sol
@@ -0,0 +1,34 @@
+
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+
+contract Transaction {
+    uint256 transactionCount;
+    event Transfer(address from , address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct{
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+        return transactions
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+}
